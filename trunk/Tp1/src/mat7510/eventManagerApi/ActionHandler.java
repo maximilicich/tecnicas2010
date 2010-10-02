f289dd413ec67f1d4b5912d0734dd9a50193539c
adri.degregorio <adri.degregorio@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-02 17:55:36 +0000

    Implementacion del EventManager, falta aun la logica del marcado y desmarcado de eventos, estoy en eso ahora

--- trunk/Tp1/src/mat7510/eventManagerApi/ActionHandler.java
+++ trunk/Tp1/src/mat7510/eventManagerApi/ActionHandler.java
@@ -9,6 +9,35 @@ public class ActionHandler {
 	
 	private List<Event> events;
 	
+	private boolean order;
+	
+	
+	static public ActionHandler createActionSingle ( ActionCommand command, Event event){
+		return new ActionHandler(command ,event);
+	}
+	
+	static public ActionHandler createActionGroup ( ActionCommand command, List<Event> event){
+		return new ActionHandler(command ,event, false);
+	}
+	
+	static public ActionHandler createActionGroupOrder ( ActionCommand command, List<Event> event){
+		return new ActionHandler(command ,event, true);
+	}
+	
+	private ActionHandler ( ActionCommand command, Event event){
+		
+		this.command = command;
+		this.events.add(event);
+		setOrder(false);
+	}
+		
+	private ActionHandler ( ActionCommand command, List<Event> events, boolean b){
+		
+		this.command = command;
+		this.events = events;
+		setOrder(b);
+	}
+	    
 	
 	public void addEvento( Event event) {
 		this.events.add(event);
@@ -25,7 +54,15 @@ public class ActionHandler {
 	public ActionCommand getCommand() {
 		return command;
 	}
+
+
+	public boolean isOrder() {
+		return order;
+	}
 	
+	private void setOrder(boolean b) {
+		order = b;		
+	}
 	
 
 }
