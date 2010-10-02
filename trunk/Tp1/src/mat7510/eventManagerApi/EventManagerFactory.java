ed3a115270ddf418f794be0ff88aa27fb5a0f7d9
sergiocirasa <sergiocirasa@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-02 21:34:35 +0000

    Implemento el eventManagerFactory como un singleton

--- trunk/Tp1/src/mat7510/eventManagerApi/EventManagerFactory.java
+++ trunk/Tp1/src/mat7510/eventManagerApi/EventManagerFactory.java
@@ -1,11 +1,24 @@
 package mat7510.eventManagerApi;
 
+
 public class EventManagerFactory {
+        private static EventManager eventManager = null;
+
+        private EventManagerFactory(){
+
+        }
 
 	public static EventManager createEventManager() {
-		// TODO Auto-generated method stub
-		// ACA DEBERIA DEVOLVER UNA IMPLEMENTACION DE UN EVENT MANAGER
-		return null; 
+		if (eventManager == null) {
+                    eventManager = new ConcreteEventManager();
+                }
+		return eventManager;
 	}
 
+        public static EventManager getInstance(){
+                 if (eventManager == null)
+                     return createEventManager();
+
+                 return eventManager;
+        }
 }
