b5f36cfb0c5a58936744bd18befe94cff4ac3471
maximilianomilicich <maximilianomilicich@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-03 20:35:22 +0000

    Primer JUnit test : FALLO !!!! Hay que arreglar algo en la implementacion !!

--- trunk/Tp1/src/mat7510/eventManagerApi/EventManager.java
+++ trunk/Tp1/src/mat7510/eventManagerApi/EventManager.java
@@ -8,5 +8,6 @@ public interface EventManager extends EventListener {
 	public void register(ActionCommand cmd, List<Event> e);
 	public void registerWithOrder(ActionCommand cmd, List<Event> e);
 	public void registerCancellables(Event event1, Event event2	);
-	
+
+	public void reset();
 }
