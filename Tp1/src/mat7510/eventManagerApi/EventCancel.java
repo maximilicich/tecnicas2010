1724e9823a5ba30a7a1692352c86745672b25806
sergiocirasa <sergiocirasa@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-04 04:25:12 +0000

    Vuelvo a la version simple y dejo comentada la version de mutuamente cancelables

--- Tp1/src/mat7510/eventManagerApi/EventCancel.java
+++ Tp1/src/mat7510/eventManagerApi/EventCancel.java
@@ -31,3 +31,36 @@ public class EventCancel {
 
 
 }
+/* //Version mutuamente cancelable
+public class EventCancel {
+	
+	private Event eventSource1;	
+	private Event eventSource2;
+
+	public EventCancel ( Event eventSource1 , Event eventSource2){
+		this.eventSource1 = eventSource1;
+		this.eventSource2 = eventSource2;
+	}
+
+        public boolean isEventCancelable(Event event){
+
+            if(eventSource2.equals(event) || eventSource1.equals(event))
+                return true;
+
+            return false;
+        }
+
+
+	public Event getEventToBeCancel(Event event) {
+
+                if(eventSource2.equals(event))
+                    return eventSource1;
+                else if(eventSource1.equals(event))
+                    return eventSource2;
+                
+		return null;
+	}
+
+
+}
+*/
