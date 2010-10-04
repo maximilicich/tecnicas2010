394da11ef58548b752da7f8cfb4a78aaa7c6a68f
sergiocirasa <sergiocirasa@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-04 01:45:24 +0000

    Test:multiples eventos en orden. No lo cumple

---
+++ Tp1/src/mat7510/eventManagerApi/test/MultipleEventsWithOrderTest.java
@@ -0,0 +1,141 @@
+
+package mat7510.eventManagerApi.test;
+
+import java.util.*;
+import mat7510.eventManagerApi.ActionCommand;
+import mat7510.eventManagerApi.Event;
+import mat7510.eventManagerApi.EventListener;
+import mat7510.eventManagerApi.EventManager;
+
+
+/**
+ *
+ * @author sergio
+ */import mat7510.eventManagerApi.EventManagerFactory;
+
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class BasicEvent1 implements Event {
+
+	public boolean equals(Event anotherEvent) {
+		// Asumimos que todo evento de esta clase es el mismo
+		if (!(anotherEvent instanceof BasicEvent1)) {
+			return false;
+		}
+		return true;
+	}
+}
+
+class BasicEvent2 implements Event {
+
+	public boolean equals(Event anotherEvent) {
+		// Asumimos que todo evento de esta clase es el mismo
+		if (!(anotherEvent instanceof BasicEvent2)) {
+			return false;
+		}
+		return true;
+	}
+}
+
+class BasicEvent3 implements Event {
+
+	public boolean equals(Event anotherEvent) {
+		// Asumimos que todo evento de esta clase es el mismo
+		if (!(anotherEvent instanceof BasicEvent3)) {
+			return false;
+		}
+		return true;
+	}
+}
+
+class BasicEventSource {
+	private EventListener listener;
+	public void addListener(EventListener listener) {
+		this.listener = listener;
+	}
+	public void triggerEvent1() {
+		listener.eventOccurred(new BasicEvent1());
+	}
+        public void triggerEvent2() {
+		listener.eventOccurred(new BasicEvent2());
+	}
+        public void triggerEvent3() {
+		listener.eventOccurred(new BasicEvent3());
+	}
+}
+
+class BasicActionReceiver {
+
+	private boolean state = false;
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+
+	public boolean getState() {
+		return state;
+	}
+
+}
+
+class BasicAction implements ActionCommand {
+
+	BasicActionReceiver receiver;
+
+	public BasicAction(BasicActionReceiver receiver) {
+		this.receiver = receiver;
+	}
+
+	public void execute() {
+		receiver.setState(true);
+	}
+
+}
+
+public class MultipleEventsWithOrderTest {
+
+	private EventManager mngr;
+	private BasicEventSource eventSource;
+	private BasicActionReceiver actionReceiver;
+
+	@Before
+	public void setUp() throws Exception {
+		mngr = EventManagerFactory.getInstance();
+
+		eventSource = new BasicEventSource();
+		eventSource.addListener(mngr);
+
+		actionReceiver = new BasicActionReceiver();
+	}
+
+	@After
+	public void tearDown() {
+		// nada por ahora...
+	}
+
+	@Test
+	public void testBasicContext() {
+
+		// Registramos en el Manager la accion - evento
+                List<Event>events = new ArrayList<Event>();
+                events.add(new BasicEvent1());
+                events.add(new BasicEvent2());
+                events.add(new BasicEvent3());
+
+		mngr.registerWithOrder(new BasicAction(actionReceiver), events);
+
+		// El Source dispara el Evento...
+		eventSource.triggerEvent1();
+                eventSource.triggerEvent3();
+                eventSource.triggerEvent2();        
+
+		// Y si todo funciona bien, el Receiver deberia haber sufrido
+		// el cambio de estado, por la accion ejecutada...
+		assertTrue(actionReceiver.getState());
+	}
+}
