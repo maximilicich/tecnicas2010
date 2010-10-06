6ba8a7c97c634e93a84655c3fad09260bc1e24a9
adri.degregorio <adri.degregorio@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-06 00:58:46 +0000

    Test de excepciones funcionando 100%

--- trunk/Tp1/src/mat7510/eventManagerApi/test/NullTriggerEvent.java
+++ trunk/Tp1/src/mat7510/eventManagerApi/test/NullTriggerEvent.java
@@ -9,57 +9,83 @@ import mat7510.eventManagerApi.Event;
 import mat7510.eventManagerApi.EventManager;
 import mat7510.eventManagerApi.EventManagerFactory;
 import mat7510.eventManagerApi.exceptionRegisterEvent;
-import mat7510.eventManagerApi.domainExamples.basicDomain.BasicActionCommand;
-import mat7510.eventManagerApi.domainExamples.basicDomain.BasicActionReceiver;
 import mat7510.eventManagerApi.domainExamples.basicDomain.BasicEvent;
 import mat7510.eventManagerApi.domainExamples.basicDomain.BasicEventSource;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class NullTriggerEvent {
 	
-	
 	private static final String EVENTO1 = "evento 1";
-
 	
 	private EventManager mngr;
 	private BasicEventSource eventSource1;
 
-	private BasicActionReceiver actionReceiver;
-
-
+	@Before
 	public void setUp() throws Exception {
-		
 		mngr = EventManagerFactory.getInstance();
 
-		eventSource1 = new BasicEventSource(EVENTO1);
-		
+		eventSource1 = new BasicEventSource(EVENTO1);		
 		eventSource1.addListener(mngr);
+	}
 
-		actionReceiver = new BasicActionReceiver();
+	@After
+	public void tearDown() {
+		// nada por ahora...
 	}
 
 	@Test
 	public void testBasicContext() throws exceptionRegisterEvent {
 
-		// Registramos en el Manager la accion - evento
-                List<Event>events = new ArrayList<Event>();
-                events.add(new BasicEvent(EVENTO1));
-
-               try{
-                   mngr.register(new BasicActionCommand(actionReceiver), events);
-               }catch(exceptionRegisterEvent e){
+            // Registramos en el Manager la accion - evento
+            List<Event>events = null;
+            Event eventTest = null;
+            
+            try{
+                mngr.registerWithOrder(null, events);
+                fail("Excepted exeptionRegisterEvent");
+            }catch(exceptionRegisterEvent e){
                    System.out.println(e.toString());
-               }
-		// El Source dispara el Evento...
-               try{
-            	   eventSource1.triggerEventNull();
-               }catch(exceptionRegisterEvent e){
+            }
+            
+          //Se verifica la validación del registro de eventos con orden
+            try{
+                mngr.registerWithOrder(null, null);
+                fail("Excepted exeptionRegisterEvent");
+            }catch(exceptionRegisterEvent e){
                    System.out.println(e.toString());
-               }          
-		
-
+            }
+            
+            //Se verifica la validación del registro de eventos sin orden
+            try{
+                mngr.register(null, events);
+                fail("Excepted exeptionRegisterEvent");
+            }catch(exceptionRegisterEvent e){
+                   System.out.println(e.toString());
+            }
+            
+            
+            //Se verifica la validación del registro de evento simple
+            try{
+                mngr.register(null, eventTest);
+                fail("Excepted exeptionRegisterEvent");
+            }catch(exceptionRegisterEvent e){
+                   System.out.println(e.toString());
+            }
+            
+            
+          //Se verifica la validación de disparo de evento nulo
+            try{
+            	eventSource1.triggerEventNull();
+            	fail("Excepted exeptionRegisterEvent");
+            }
+            catch(exceptionRegisterEvent e){
+                   System.out.println(e.toString());
+            }  
+            
 	}
 
 }
