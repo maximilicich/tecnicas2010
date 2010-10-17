a33465354f880289d9fea38b5fec4a2597f48577
maximilianomilicich <maximilianomilicich@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-17 23:45:21 +0000

    Mas Test Unitarios para los EventChains (todos dan verde!!)

---
+++ trunk/Tp1/src/mat7510/eventManagerApi/version2/OrderedEventChainFilter.java
@@ -0,0 +1,89 @@
+package mat7510.eventManagerApi.version2;
+
+import java.util.Iterator;
+
+/**
+ * 
+ * @author Grupo 10 
+ *
+ */
+public class OrderedEventChainFilter extends EventChainFilter {
+
+	public OrderedEventChainFilter(EventChain filteredChain) {
+		super(filteredChain);
+	}
+
+	/**
+	 * Recordar que un filtro ante un evento ocurrido
+	 * O bien sigue la cadena (haciendo super.eventOccurred(e)
+	 * O bien corta la cadena (haciendo simplemente return)
+	 * 
+	 */
+	public void eventOccurred(Event e) {
+
+		Iterator<Element> iterator = this.iterator();
+		
+		// Si la cadena esta vacia, entonces no se cumple el orden
+		// Nos vamos reseteando la  
+		if (! iterator.hasNext()) {
+			resetOccurrences();
+			return;
+		}
+		
+		// Ahora buscamos el proximo evento esperado 
+		// (el primero aun no occurrido) 
+		Event nextEventExpected = null;
+		while (iterator.hasNext() && nextEventExpected == null) {
+			Element element = iterator.next();
+			if (! element.hasOccurred())
+				nextEventExpected = element.getEvent();
+		}
+		
+		// Si no hay proximo evento esperado (ya occurrieron todos)
+		// El orden se cumple...(?)
+		// Entonces Nos vamos siguiendo la cadena
+		if (nextEventExpected == null) {
+			super.eventOccurred(e);
+			return;
+		}
+		
+		// Si el proximo evento esperado es el evento occurrido
+		// Entonces se cumple el Orden:
+		// Nos vamos siguiendo la cadena
+		if (nextEventExpected.equals(e)) {
+			super.eventOccurred(e);
+			return;
+		}
+		
+		// Sino, verificamos que ninguno de los proximos eventos esperados
+		// sea el ocurrido
+		// si asi fuera, entonces no se cumple el Orden
+		while (iterator.hasNext()) {
+			Element element = iterator.next();
+			if (element.getEvent().equals(e)) {
+				// El evento ocurrido esta entre los esperados
+				// Pero No se cumple el orden!
+				// Reseteamos las occurrencias y nos vamos cortando la cadena!
+				resetOccurrences();
+				return;
+			}
+			if (element.hasOccurred()) {
+				// Caso especial:
+				// Encontramos un evento marcado como ocurrido
+				// Luego de un evento no ocurrido
+				// Esto es señal de que la cadena NO esta marcada En Orden
+				// Reseteamos las occurrencias y nos vamos cortando la cadena!
+				resetOccurrences();
+				return;
+			}
+		}
+		
+		// Si llegamos aca, entonces El evento ocurrido No esta entre los esperados
+		// Asi, Seguimos la cadena...
+		super.eventOccurred(e);
+		
+	}
+
+	
+	
+}
---
+++ trunk/Tp1/src/mat7510/eventManagerApi/version2/test/ContinuousOrderedChainFilterTest.java
@@ -0,0 +1,122 @@
+package mat7510.eventManagerApi.version2.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import mat7510.eventManagerApi.version2.ActionEventChain;
+import mat7510.eventManagerApi.version2.ContinuousEventChainFilter;
+import mat7510.eventManagerApi.version2.EventChain;
+import mat7510.eventManagerApi.version2.OrderedEventChainFilter;
+import mat7510.eventManagerApi.version2.domainExamples.basicDomain.BasicActionCommand;
+import mat7510.eventManagerApi.version2.domainExamples.basicDomain.BasicActionReceiver;
+import mat7510.eventManagerApi.version2.domainExamples.basicDomain.BasicEvent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ContinuousOrderedChainFilterTest {
+
+	private BasicActionReceiver actionReceiver;
+	private EventChain chain;
+	private final String ASSERT_MSG_PREFIX = "[A-B-C Ordenado y Continuo] ";
+	
+	@Before
+	public void setUp() {
+		
+		actionReceiver = new BasicActionReceiver();
+		
+		chain = new ContinuousEventChainFilter(new OrderedEventChainFilter(new ActionEventChain(new BasicActionCommand(actionReceiver))));
+		chain.addEvent(new BasicEvent("A"));
+		chain.addEvent(new BasicEvent("B"));
+		chain.addEvent(new BasicEvent("C"));
+
+	}
+	
+	@Test
+	public void testOrderedContinuousChain() {
+		
+		chain.eventOccurred(new BasicEvent("A"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio A y la accion ya ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("B"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio A-B y la accion ya ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("C"));
+		assertTrue(ASSERT_MSG_PREFIX + "ocurrio A-B-C y la accion no ocurrio!",actionReceiver.getState());
+		
+	}
+
+	@Test
+	public void testUnorderedContinuousChain() {
+		
+		chain.eventOccurred(new BasicEvent("B"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio B y la accion ya ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("C"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio B-C y la accion ya ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("A"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio B-C-A y la accion ocurrio!",actionReceiver.getState());
+		
+	}
+
+	
+	@Test
+	public void testOrderedDiscontinuousChain() {
+		
+		chain.eventOccurred(new BasicEvent("A"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio A y la accion ya ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("B"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio A-B y la accion ya ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("K"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio A-B-K y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("C"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio A-B-K-C y la accion ocurrio!",actionReceiver.getState());
+		
+	}
+
+	
+	@Test
+	public void testUnorderedDiscontinuousChain() {
+		
+		chain.eventOccurred(new BasicEvent("B"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio B y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("K"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio B-K y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("A"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio B-K-A y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("C"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio B-K-A-C y la accion ocurrio!",actionReceiver.getState());
+		
+	}
+
+	
+	@Test
+	public void testOrderedContinuousChainResetAfterActionTriggered() {
+		
+		chain.eventOccurred(new BasicEvent("A"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio A y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("B"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio AB y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("C"));
+		assertTrue(ASSERT_MSG_PREFIX + "ocurrio ABC y la accion no ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("A"));
+		assertTrue(ASSERT_MSG_PREFIX + "ocurrio A luego de ABC y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("B"));
+		assertTrue(ASSERT_MSG_PREFIX + "ocurrio AB luego de ABC y la accion ocurrio!",actionReceiver.getState());
+		
+		chain.eventOccurred(new BasicEvent("C"));
+		assertFalse(ASSERT_MSG_PREFIX + "ocurrio ABC luego de ABC y la accion no ocurrio!",actionReceiver.getState());
+	}
+
+}
