fc6013974a6f545e1ece82f7238ba5c2eec88905
adri.degregorio <adri.degregorio@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-20 03:31:40 +0000

    Refactor de los filtros, funcionando sin problemas

--- Tp1/src/mat7510/eventManagerApi/version2/OrderedEventChainFilter.java
+++ Tp1/src/mat7510/eventManagerApi/version2/OrderedEventChainFilter.java
@@ -24,64 +24,55 @@ public class OrderedEventChainFilter extends EventChainFilter {
 		Iterator<Element> iterator = this.iterator();
 		
 		// Si la cadena esta vacia, entonces no se cumple el orden
-		// Nos vamos reseteando la  
 		if (! iterator.hasNext()) {
 			resetOccurrences();
 			return;
 		}
 		
-		// Ahora buscamos el proximo evento esperado 
-		// (el primero aun no occurrido) 
-		Event nextEventExpected = null;
-		while (iterator.hasNext() && nextEventExpected == null) {
-			Element element = iterator.next();
-			if (! element.hasOccurred())
-				nextEventExpected = element.getEvent();
-		}
+		Event nextEventExpected = searchNextEventExpected(iterator);
 		
 		// Si no hay proximo evento esperado (ya occurrieron todos)
-		// El orden se cumple...(?)
-		// Entonces Nos vamos siguiendo la cadena
 		if (nextEventExpected == null) {
 			super.eventOccurred(e);
 			return;
 		}
 		
 		// Si el proximo evento esperado es el evento occurrido
-		// Entonces se cumple el Orden:
-		// Nos vamos siguiendo la cadena
 		if (nextEventExpected.equals(e)) {
 			super.eventOccurred(e);
 			return;
 		}
 		
-		// Sino, verificamos que ninguno de los proximos eventos esperados
-		// sea el ocurrido
+		// Se verifica que ninguno de los proximos eventos esperados sea el ocurrido
 		// si asi fuera, entonces no se cumple el Orden
 		while (iterator.hasNext()) {
 			Element element = iterator.next();
 			if (element.getEvent().equals(e)) {
-				// El evento ocurrido esta entre los esperados
-				// Pero No se cumple el orden!
-				// Reseteamos las occurrencias y nos vamos cortando la cadena!
+				// El evento ocurrido esta entre los esperados pero no cumple el orden
 				resetOccurrences();
 				return;
 			}
 			if (element.hasOccurred()) {
-				// Caso especial:
-				// Encontramos un evento marcado como ocurrido
-				// Luego de un evento no ocurrido
-				// Esto es señal de que la cadena NO esta marcada En Orden
-				// Reseteamos las occurrencias y nos vamos cortando la cadena!
+				// Evento marcado como ocurrido luego de un evento no ocurrido
+				// La cadena NO esta marcada En Orden
 				resetOccurrences();
 				return;
 			}
 		}
 		
-		// Si llegamos aca, entonces El evento ocurrido No esta entre los esperados
-		// Asi, Seguimos la cadena...
+		// El evento ocurrido No esta entre los esperados
 		super.eventOccurred(e);
 		
+	}
+
+	private Event searchNextEventExpected(Iterator<Element> iterator) {
+		Event nextEventExpected = null;
+		while (iterator.hasNext() && nextEventExpected == null) {
+			Element element = iterator.next();
+			if (! element.hasOccurred())
+				nextEventExpected = element.getEvent();
+		}
+		return nextEventExpected;
 	}
 
 	
