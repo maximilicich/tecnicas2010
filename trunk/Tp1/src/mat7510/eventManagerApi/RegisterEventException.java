229ffe008298232c838a07962dbeec23d76bc912
maximilianomilicich <maximilianomilicich@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-10-21 09:05:16 +0000

    INFORME LISTO Faltarian hacer diagramas de secuencia... SE corrigieron cosas menores en el codigo (testeado con JUnit)

--- trunk/Tp1/src/mat7510/eventManagerApi/RegisterEventException.java
+++ trunk/Tp1/src/mat7510/eventManagerApi/RegisterEventException.java
@@ -8,13 +8,9 @@ package mat7510.eventManagerApi;
 public class RegisterEventException  extends Exception{
 
 	private static final long serialVersionUID = 1L;
-	private String text;
+	
 	public RegisterEventException(String e) {
-		text = "No se puede registrar los eventos debido a: " + e;
+		super("No se puede registrar los eventos debido a: " + e);
 	}
 
-	@Override
-	public String toString() {
-		return text;
-	}
 }
