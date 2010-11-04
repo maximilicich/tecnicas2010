d0d7db0094d9ea1213e41474a331692fd41c6221
adri.degregorio <adri.degregorio@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-11-04 00:47:48 +0000

    Subo mas unit test para el actionHandler

--- Tp1/src/mat7510/eventManagerApi/test/ActionHandlerTest.java
+++ Tp1/src/mat7510/eventManagerApi/test/ActionHandlerTest.java
@@ -21,6 +21,7 @@ public class ActionHandlerTest{
 	
 	
 	private ActionHandler actionhandler;
+	private ActionHandler actionhandlerContinuos;
 	private BasicActionReceiver receiver;
 	private BasicActionCommand cmd;
 	private static final String EVENTO0 = "A";
@@ -39,6 +40,7 @@ public class ActionHandlerTest{
 		events.add(new BasicEvent(EVENTO2));
 		
 		actionhandler = ActionHandler.createActionGroupDiscontinuousWithCancellations(cmd,events);
+		actionhandlerContinuos = ActionHandler.createActionGroupOrderContinousWithNoCancellations(cmd,events);
 		
 	}
 	
@@ -59,6 +61,8 @@ public class ActionHandlerTest{
 	public void testCancelEvent() {
 		
 		int index = 1;	
+		actionhandler.activateEvent(index);
+		assertTrue(actionhandler.isActivedEvent(index));
 		actionhandler.cancelEvent(index);
 		assertFalse(actionhandler.isActivedEvent(index));
 		index = 0;
@@ -67,6 +71,105 @@ public class ActionHandlerTest{
 		assertFalse(actionhandler.isActivedEvent(index));
 
 	}
+	
+	@Test
+	public void testNotifyCancelEventDiscontinous(){
+		
+		int index = 1;	
+		actionhandler.activateEvent(index);
+		assertTrue(actionhandler.isActivedEvent(index));
+		
+		actionhandler.notifyCancelEvent(new BasicEvent(EVENTO1));
+		assertFalse(actionhandler.isActivedEvent(index));
+		
+		index = 0;
+		assertFalse(actionhandler.isActivedEvent(index));
+		index = 2;
+		assertFalse(actionhandler.isActivedEvent(index));
+				
+	}
+	
+	@Test
+	public void testNotifyCancelEventContinous(){
+		
+		int index = 1;
+		
+		for ( index = 0 ; index < 3; index ++){
+			actionhandlerContinuos.activateEvent(index);
+			assertTrue(actionhandlerContinuos.isActivedEvent(index));
+		}
+		
+		actionhandlerContinuos.notifyCancelEvent(new BasicEvent(EVENTO1));
+
+		for ( index = 0 ; index < 3; index ++){
+			assertFalse(actionhandlerContinuos.isActivedEvent(index));
+	    }
+						
+	}
+	
+	@Test
+	public void testNotifyEventContinuosOK(){
+		
+		int index = 0;
+		
+		actionhandlerContinuos.notifyEvent(new BasicEvent(EVENTO0));
+	    assertTrue(actionhandlerContinuos.isActivedEvent(index));
+		
+	    index = 1;
+	    actionhandlerContinuos.notifyEvent(new BasicEvent(EVENTO1));
+	    assertTrue(actionhandlerContinuos.isActivedEvent(index));
+	    
+	    index = 2;
+	    actionhandlerContinuos.notifyEvent(new BasicEvent(EVENTO2));
+		
+	    for ( index = 0 ; index < 3; index ++){
+			assertFalse(actionhandlerContinuos.isActivedEvent(index));
+	    }
+	    
+	}
+	
+	@Test
+	public void testNotifyEventContinuosNoOK(){
+		
+		int index = 1;
+	    
+	    actionhandlerContinuos.notifyEvent(new BasicEvent(EVENTO1));
+	    assertFalse(actionhandlerContinuos.isActivedEvent(index));
+		
+	    index = 0;
+	    actionhandlerContinuos.notifyEvent(new BasicEvent(EVENTO0));
+	    assertTrue(actionhandlerContinuos.isActivedEvent(index));
+	    
+	    index = 2;
+	    actionhandlerContinuos.notifyEvent(new BasicEvent(EVENTO2));
+	    
+	    for ( index = 0 ; index < 3; index ++){
+			assertFalse(actionhandlerContinuos.isActivedEvent(index));
+	    }
+	    
+	}    
+	
+	@Test
+	public void testNotifyEventDiscontinuos(){
+		
+		int index = 1;
+	    
+	    actionhandler.notifyEvent(new BasicEvent(EVENTO1));
+	    assertTrue(actionhandler.isActivedEvent(index));
+		
+	    index = 0;
+	    actionhandler.notifyEvent(new BasicEvent(EVENTO0));
+	    assertTrue(actionhandler.isActivedEvent(index));
+	    
+	    index = 2;
+	    actionhandler.notifyEvent(new BasicEvent(EVENTO2));
+	    
+	    for ( index = 0 ; index < 3; index ++){
+			assertFalse(actionhandler.isActivedEvent(index));
+	    }
+	    
+	}    
+	
 	
 
 }
