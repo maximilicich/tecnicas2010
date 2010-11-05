0cd1a42c56cad9b91cafede74fccce9081f06d7a
maximilianomilicich <maximilianomilicich@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-11-05 03:18:49 +0000

    GenericDeviceDriver, DeviceAction y DeviceEvent... Los pilares del SmartBuilding !

---
+++ Tp1/src/mat7510/smartBuilding/DeviceAction.java
@@ -0,0 +1,28 @@
+package mat7510.smartBuilding;
+
+import mat7510.eventManagerApi.version2.ActionCommand;
+
+/**
+ * Representa una Accion posible a ejecutar por un Device (dispositivo)
+ * Esta accion extiende el concepto de ActionCommand segun la API eventManager
+ * 
+ * En SmartBuilding la interaccion entre dispositivos se configura
+ * relacionando Acciones con Eventos
+ * Las Acciones deben ser DeviceActions
+ * Los Eventos deben ser DeviceEventS
+ *  
+ * @author Grupo 10
+ *
+ */
+public interface DeviceAction extends ActionCommand {
+
+	/**
+	 * Toda Accion debe proveer su nombre, para 
+	 * que el Usuario pueda identificarla en la GUI
+	 * y asi configurar la interaccion entre Devices
+	 * 
+	 * @return el nombre de la accion, tal como se le debe mostrar al Usuario final
+	 */
+	public String getActionName();
+	
+}
---
+++ Tp1/src/mat7510/smartBuilding/DeviceEvent.java
@@ -0,0 +1,28 @@
+package mat7510.smartBuilding;
+
+import mat7510.eventManagerApi.version2.Event;
+
+/**
+ * Representa un Evento informado por un dispositivo al ambiente
+ * Este evento extiende el concepto de Evento segun la API eventManager
+ * 
+ * En SmartBuilding la interaccion entre dispositivos se configura
+ * relacionando Acciones con Eventos
+ * Las Acciones deben ser DeviceActions
+ * Los Eventos deben ser DeviceEventS
+ * 
+ * @author Grupo 10
+ *
+ */
+public interface DeviceEvent extends Event {
+
+	/**
+	 * Todo evento debe proveer su nombre, para 
+	 * que el Usuario pueda identificarlo en la GUI
+	 * y asi configurar la interaccion entre Devices
+	 * 
+	 * @return el nombre del evento, tal como se le debe mostrar al Usuario final
+	 */
+	public String getEventName();
+	
+}
