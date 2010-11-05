9f5e332763ef3d56e66ca9ffbf5d773f696ab406
maximilianomilicich <maximilianomilicich@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-11-05 03:18:49 +0000

    GenericDeviceDriver, DeviceAction y DeviceEvent... Los pilares del SmartBuilding !

--- trunk/Tp1/src/mat7510/smartBuilding/GenericDeviceDriver.java
+++ trunk/Tp1/src/mat7510/smartBuilding/GenericDeviceDriver.java
@@ -1,9 +1,8 @@
 package mat7510.smartBuilding;
 
 import java.util.List;
+import java.util.Map;
 
-import mat7510.eventManagerApi.version2.ActionCommand;
-import mat7510.eventManagerApi.version2.Event;
 import mat7510.eventManagerApi.version2.EventListener;
 
 /**
@@ -14,17 +13,18 @@ import mat7510.eventManagerApi.version2.EventListener;
  * Este Driver generico interactua con la API controladora de Eventos (eventManagerAPI)
  * 
  * Todo driver de dispositivo debe proveeer 
- * a) Las {@link mat7510.eventManagerApi.version2.ActionCommand acciones } que pueden realizarse sobre el dispositivo
- * b) Los {@link mat7510.eventManagerApi.version2.Event eventos } que el dispositivo informa al entorno
- * c) Los estados que posee el dispositivo (estados?)
- * d) Un modo de informar el valor actual de cada estado
+ * a) Las {@link mat7510.smartBuilding.DeviceAction acciones } que pueden realizarse sobre el dispositivo
+ * b) Los {@link mat7510.smartBuilding.DeviceEvent eventos } que el dispositivo informa al entorno
+ * c) El estado actual del dispositivo: El estado se define por un conjunto 
+ * 	  de nombres de atributos y su valor actual correspondiente 
+ * 	  Estos valores son siempre de tipo String
  * 
  *  Asimismo, debe asegurar un modo para suscribir uno o mas {@link EventListener mat7510.eventManagerApi.version2.EventListener } 
  *  Los cuales deberan recibir los Eventos emitidos por el dispositivo
  *  
  *  El modo de ejecutar una accion sobre el dispositivo es:
- *  1. obtener con getActionCommand() el objeto {@link mat7510.eventManagerApi.version2.ActionCommand Accion } a partir del nombre de la accion
- *  2. ejecutar el objeto accion (actionCommand.execute())
+ *  1. identificar la Accion a ejecutar en la lista devuelta por getActions() 
+ *  2. ejecutar el objeto accion (deviceAction.execute())
  *  
  * @author Grupo 10 
  *
@@ -32,46 +32,35 @@ import mat7510.eventManagerApi.version2.EventListener;
 public interface GenericDeviceDriver {
 
 	/**
+	 * Las {@link mat7510.smartBuilding.DeviceAction acciones } que pueden realizarse sobre el dispositivo
 	 * 
 	 * @return
 	 */
-	List<String> getActions();
+	List<DeviceAction> getActions();
 	
 	/**
+	 * Los {@link mat7510.smartBuilding.DeviceEvent eventos } que el dispositivo informa al entorno
 	 * 
 	 * @return
 	 */
-	List<String> getEvents();
+	List<DeviceEvent> getEvents();
 	
 	/**
+	 * El estado actual del dispositivo: El estado se define por un conjunto 
+	 *  de nombres de atributos y su valor actual correspondiente 
+	 *  Estos valores son siempre de tipo String
+	 *  
+	 * @return un Mapa de pares ordenados (nombre, valor)
+	 * en donde cada par ordenado corresponde a un atributo del dispositivo
 	 * 
-	 * @return
 	 */
-	List<String> getStates();
+	Map<String, String> getState();
+	
 	
 	/**
-	 * 
-	 * @param actionName
-	 * @return
-	 */
-	ActionCommand getAction(String actionName);
-	
-	/**
-	 * 
-	 * @param eventName
-	 * @return
-	 */
-	Event getEvent(String eventName);
-	
-	/**
-	 * 
-	 * @param state
-	 * @return
-	 */
-	String getStateValue(String state);
-	
-	/**
-	 * 
+	 *  Metodo para suscribir uno o mas {@link EventListener mat7510.eventManagerApi.version2.EventListener } 
+	 *  Los cuales deberan recibir los Eventos emitidos por el dispositivo
+	 *  
 	 * @param eventListener
 	 */
 	void addEventListener(EventListener eventListener);
