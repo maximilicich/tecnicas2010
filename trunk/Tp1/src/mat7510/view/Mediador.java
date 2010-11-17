e5b5949b10979fa1459f15a3c43e54981922f969
sergiocirasa <sergiocirasa@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-11-17 02:24:08 +0000

    organizo los paneles

--- trunk/Tp1/src/mat7510/view/Mediador.java
+++ trunk/Tp1/src/mat7510/view/Mediador.java
@@ -21,62 +21,83 @@ public class Mediador {
         createWindow();
     }
 
+    private JPanel createContentPanel(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(new EmptyBorder(5,5,5,5));
+        return panel;
+    }
+
+    private JPanel createControllerPanel(){
+        JPanel controllerPanel = new JPanel();
+        controllerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        controllerPanel.setMaximumSize(new Dimension(400,18));
+        return controllerPanel;
+    }
+
+    private JPanel createDriverPanel(){
+        JButton updateButton = new JButton("Actualizar");
+        JPanel driversListPanel = new JPanel();
+        JPanel controllerPanel = createControllerPanel();
+        controllerPanel.add(updateButton);
+
+        JPanel driverPanel = createContentPanel();
+        driverPanel.add(driversListPanel);
+        driverPanel.add(controllerPanel);
+
+        return driverPanel;
+    }
+
+    private JPanel createStatePanel(){
+        JPanel statePanel = createContentPanel();
+        JPanel stateListPanel = new JPanel();
+        statePanel.add(stateListPanel);
+        statePanel.add(createControllerPanel());
+        return statePanel;
+    }
+
+    private JPanel createActionPanel(){       
+        JPanel actionListPanel = new JPanel();
+        JButton executeButton = new JButton("Ejecutar");
+        JPanel controlerPanel = createControllerPanel();
+        controlerPanel.add(executeButton);
+
+         JPanel actionPanel = createContentPanel();
+        actionPanel.add(actionListPanel);
+        actionPanel.add(executeButton);
+        return actionPanel;
+    }
+
+    private JPanel createEventPanel(){
+        JButton deleteButton = new JButton("Eliminar");
+        JButton addButton = new JButton("Agregar");
+        JButton detailButton = new JButton("Descripcion");
+
+        JPanel controllerEventPanel = createControllerPanel();
+        controllerEventPanel.add(addButton);
+        controllerEventPanel.add(deleteButton);
+        controllerEventPanel.add(detailButton);
+
+        JPanel eventListPanel = new JPanel();
+
+        JPanel eventPanel = createContentPanel();
+        eventPanel.add(eventListPanel);
+        eventPanel.add(controllerEventPanel);
+        return eventPanel;
+    }
+
     private void createWindow(){
 
         JPanel contentPane = new JPanel();
         mainFrame.getContentPane().add(contentPane);
         contentPane.setLayout(new BorderLayout());
 
-        JButton updateButton = new JButton("Actualizar");
-        JPanel driversListPanel = new JPanel();
-
-        JPanel driverPanel = new JPanel();
-        driverPanel.setBorder(new EmptyBorder(5,5,5,5));
-        driverPanel.setLayout(new BoxLayout(driverPanel, BoxLayout.Y_AXIS));
-        driverPanel.add(driversListPanel);
-        driverPanel.add(updateButton);
-
-        JPanel statePanel = new JPanel();
-        statePanel.setBorder(new EmptyBorder(5,5,5,5));
-
-        JButton executeButton = new JButton("Ejecutar");
-        JPanel actionListPanel = new JPanel();
-
-        JPanel actionPanel = new JPanel();
-        actionPanel.setLayout(new BoxLayout(actionPanel, BoxLayout.Y_AXIS));
-        actionPanel.setBorder(new EmptyBorder(5,5,5,5));
-        actionPanel.add(actionListPanel);
-        actionPanel.add(executeButton);
-
-        
-        JPanel eventListPanel = new JPanel();
-       
-
-        JButton deleteButton = new JButton("Eliminar");
-        JButton addButton = new JButton("Agregar");
-        JButton detailButton = new JButton("Descripcion");
-
-        JPanel controlerEventPanel = new JPanel();
-        controlerEventPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        controlerEventPanel.setMaximumSize(new Dimension(500,20));
-
-        controlerEventPanel.add(addButton);
-        controlerEventPanel.add(deleteButton);
-        controlerEventPanel.add(detailButton);
-        
-
-        JPanel eventPanel = new JPanel();
-        eventPanel.setBorder(new EmptyBorder(5,5,5,5));
-        eventPanel.setLayout(new BoxLayout(eventPanel, BoxLayout.Y_AXIS));
-        eventPanel.add(eventListPanel);
-        eventPanel.add(controlerEventPanel);
-
-        JPanel centerPanel = new JPanel();
+         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new GridLayout(1, 4));
-        centerPanel.add(driverPanel);
-        centerPanel.add(statePanel);
-        centerPanel.add(actionPanel);
-        centerPanel.add(eventPanel);
+        centerPanel.add(createDriverPanel());
+        centerPanel.add(createStatePanel());
+        centerPanel.add(createActionPanel());
+        centerPanel.add(createEventPanel());
         contentPane.add(centerPanel, BorderLayout.CENTER);
 
         JPanel topPanel = new JPanel();
@@ -86,9 +107,6 @@ public class Mediador {
         topPanel.add(new JLabel("Acciones", JLabel.CENTER));
         topPanel.add(new JLabel("Eventos", JLabel.CENTER));
         contentPane.add(topPanel, BorderLayout.NORTH);
-
-        topPanel.setBackground(Color.red);
-        controlerEventPanel.setBackground(Color.blue);
 
     }
     
