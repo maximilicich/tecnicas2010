c33fe2a826f3890336d1f7579a40edc030523708
maximilianomilicich <maximilianomilicich@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-11-23 06:57:33 +0000

    VALIDACION DE ID DUPLICADO PARA LAS CLASES DE DOMINIO No estaba funcionando bien. Ahora si

--- trunk/Tp1/src/mat7510/smartBuilding/test/XMLTranslatorTest.java
+++ trunk/Tp1/src/mat7510/smartBuilding/test/XMLTranslatorTest.java
@@ -2,6 +2,8 @@ package mat7510.smartBuilding.test;
 
 import java.io.ByteArrayInputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import mat7510.smartBuilding.model.DeviceDriver;
 import mat7510.smartBuilding.model.SmartBuildingException;
@@ -26,6 +28,38 @@ public class XMLTranslatorTest {
 		System.out.println(dd.getDeviceID());
 		System.out.println(dd.getDeviceDescription());
 		System.out.println(dd.getClass().getName());
+
+		DeviceDriver dd2 = xt.getDeviceDriver(new ByteArrayInputStream(xml.getBytes("UTF-8")));
+
+		if (dd.equals(dd2))
+			System.out.println("SON IGUALES");
+		else
+			System.out.println("SON DISTINTOS");
+		
+		Set<DeviceDriver> myset = new LinkedHashSet<DeviceDriver>();
+		
+		myset.add(dd);
+		myset.add(dd2);
+		for (DeviceDriver deviceDriver : myset) {
+			System.out.println(deviceDriver);
+			
+		}
+		
+		String [] strArray = new String[10];
+
+		for(int i=0;i<strArray.length;i++)
+		strArray[i] = "Element";
+
+		Set<String> s = new LinkedHashSet<String>();
+
+		for(int i=0; i<strArray.length;i++){
+		if(!s.add(strArray[i]))
+		System.out.println("Duplicate found : " + strArray[i]);
+		}
+
+		System.out.println(s.size() + " distinct words detected : " + s );
+
+		
 	}
 	
 }
