8cb459a5465f08ae948a8ccb06991e2c6d9a6b8c
maximilianomilicich <maximilianomilicich@56ef6876-a2be-6742-08df-4a6be370bd76>
2010-11-24 01:34:24 +0000

    DELETE mat7510/playground

--- trunk/Tp1/src/mat7510/playground/gui/Command.java
+++
@@ -1,6 +0,0 @@
-package mat7510.playground.gui;
-
-public interface Command
-{
-   public void Execute();
-}
--- trunk/Tp1/src/mat7510/playground/gui/Iterator.java
+++
@@ -1,9 +0,0 @@
-package mat7510.playground.gui;
-
-public interface Iterator
-{
-public Object First();
-public Object Next();
-public boolean isDone();
-public Object CurrentItem();
-}
